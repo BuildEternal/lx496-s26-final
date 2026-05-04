365c49fd057f0c89ed6960bf3e5be88eb0d25f3c
yidinghao <5271164+yidinghao@users.noreply.github.com>
2026-05-04 12:57:02 -0400

    Updated job script to BU SCC format

--- hpc/run_truthfulqa_evaluation.sh
+++
@@ -1,4 +0,0 @@
-# Add all your Python calls here:
-python truthfulqa.py facebook/opt-125m
-python truthfulqa.py facebook/opt-350m
-# ... etc.
---
+++ scc/run_truthfulqa_evaluation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+# Please modify this file as needed, and upload it to your personal SCC folder (or a subdirectory thereof) at:
+# /projectnb/lx496sh/students/your_bu_id
+
+# To submit this job, cd into the folder where you uploaded this file and run:
+# qsub run_bitfit_experiment.sh
+
+# To change the session settings, see the official SCC instructions at:
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/submitting-jobs/
+
+#$ -P lx496sh       # This job is for LX 496/796
+#$ -l h_rt=3:00:00  # Reserve the batch node for 3 hours
+#$ -l gpus=1        # Request 1 GPU
+#$ -N myjobname     # Give your job a name
+#$ -m bea           # Send you an email when the job starts and ends
+#$ -o log.txt       # Save anything printed to the terminal to a file...
+#$ -j y             # ...including both print statements and error messages
+
+module load miniconda
+module load academic-ml/spring-2026
+conda activate spring-2026-pyt
+cd /projectnb/lx496sh/students/your_bu_id  # Modify this with your information
+
+# Add all your Python calls here:
+python truthfulqa.py facebook/opt-125m
+python truthfulqa.py facebook/opt-350m
+# ... etc.
